93ea89c5477020bd27083b42287275d2ab4a50d8
Robert Jonjic <robert.jonjic@gmail.com>
2023-09-11 10:38:20 +0100

    test new folder

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -32,7 +32,7 @@ namespace CityInfo.API
             //Additional test feature
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Testing repository name change - interoperability");
+                await context.Response.WriteAsync("Repository name change - interoperability");
             });
 
             app.Run();
