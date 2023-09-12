45f17c97f56f539a063c152ba9151fa740092085
Robert Jonjic <robert.jonjic@gmail.com>
2023-09-12 15:57:59 +0100

    expanded DataStore

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -4,7 +4,16 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; }
-        public int NumberOfPointsOfInterest { get; set; }
+        public string? Description { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+        = new List<PointOfInterestDto>();
     }
 }
